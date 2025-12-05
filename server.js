bd0d74455cffeee4711c927a005ba41aa31c01b9
unknown <mmerukelvin@gmail.com>
2025-12-05 23:40:39 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve all files in this folder
+app.use(express.static(__dirname));
+
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
